157990f538fade22e8cd040c079b0ba0f3be5e93
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-06-02 21:59:47 -0400

    first push

--- js/index.js
+++ js/index.js
@@ -40,3 +40,10 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Task 1
+
+document.getElementsByTagName('div');
+
+document.getElementsByClassName('container');
